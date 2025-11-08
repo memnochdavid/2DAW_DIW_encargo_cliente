14ea23cfad791f19740561142ceceaf0e595a372
memnochdavid <davduquediaz@gmail.com>
2025-11-08 10:09:52 +0100

    index done categorias donde relacionados done faqs done catalogo done contacto done boton sesión done

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -16,3 +16,31 @@ document.addEventListener("click", (event) => {
         contenedorObjetosMenu.classList.remove("visible");
     }
 });
+
+//inicio sesión
+document.addEventListener('DOMContentLoaded', () => {
+    const dialogo = document.getElementById('dialogoUsuario');
+    const botonAbrir = document.getElementById('abrirDialogoUsuario');
+    const botonCerrar = document.getElementById('cerrarDialogoUsuario');
+
+    botonAbrir.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (dialogo) {
+            dialogo.showModal();
+        }
+    });
+
+    if (dialogo) {
+        dialogo.addEventListener('click', (e) => {
+            const rect = dialogo.getBoundingClientRect();
+            const isInDialog =
+                rect.top <= e.clientY && e.clientY <= rect.bottom &&
+                rect.left <= e.clientX && e.clientX <= rect.right;
+
+            if (!isInDialog) {
+                dialogo.close();
+            }
+
+        });
+    }
+});
